73970ad7e9becd2b970a4dd0af610a646074cf0e
Kai <kai@7mind.io>
2020-03-19 17:38:34 +0000

    Update to izumi-0.10.2

--- src/test/scala/leaderboard/zioenv.scala
+++ src/test/scala/leaderboard/zioenv.scala
@@ -3,31 +3,31 @@ package leaderboard
 import leaderboard.model._
 import leaderboard.repo.{Ladder, Profiles, Ranks}
 import org.scalacheck.Arbitrary
-import zio.{IO, URIO, ZIO}
+import zio.{Has, IO, URIO, ZIO}
 
 object zioenv {
 
   object ladder extends Ladder[ZIO[LadderEnv, ?, ?]] {
-    def submitScore(userId: UserId, score: Score): ZIO[LadderEnv, QueryFailure, Unit] = ZIO.accessM(_.ladder.submitScore(userId, score))
-    def getScores: ZIO[LadderEnv, QueryFailure, List[(UserId, Score)]]                = ZIO.accessM(_.ladder.getScores)
+    def submitScore(userId: UserId, score: Score): ZIO[LadderEnv, QueryFailure, Unit] = ZIO.accessM(_.get.submitScore(userId, score))
+    def getScores: ZIO[LadderEnv, QueryFailure, List[(UserId, Score)]]                = ZIO.accessM(_.get.getScores)
   }
 
   object profiles extends Profiles[ZIO[ProfilesEnv, ?, ?]] {
-    override def setProfile(userId: UserId, profile: UserProfile): ZIO[ProfilesEnv, QueryFailure, Unit] = ZIO.accessM(_.profiles.setProfile(userId, profile))
-    override def getProfile(userId: UserId): ZIO[ProfilesEnv, QueryFailure, Option[UserProfile]]        = ZIO.accessM(_.profiles.getProfile(userId))
+    override def setProfile(userId: UserId, profile: UserProfile): ZIO[ProfilesEnv, QueryFailure, Unit] = ZIO.accessM(_.get.setProfile(userId, profile))
+    override def getProfile(userId: UserId): ZIO[ProfilesEnv, QueryFailure, Option[UserProfile]]        = ZIO.accessM(_.get.getProfile(userId))
   }
 
   object ranks extends Ranks[ZIO[RanksEnv, ?, ?]] {
-    override def getRank(userId: UserId): ZIO[RanksEnv, QueryFailure, Option[RankedProfile]] = ZIO.accessM(_.ranks.getRank(userId))
+    override def getRank(userId: UserId): ZIO[RanksEnv, QueryFailure, Option[RankedProfile]] = ZIO.accessM(_.get.getRank(userId))
   }
 
   object rnd extends Rnd[ZIO[RndEnv, ?, ?]] {
-    override def apply[A: Arbitrary]: URIO[RndEnv, A] = ZIO.accessM(_.rnd.apply[A])
+    override def apply[A: Arbitrary]: URIO[RndEnv, A] = ZIO.accessM(_.get.apply[A])
   }
 
-  trait LadderEnv { def ladder: Ladder[IO] }
-  trait ProfilesEnv { def profiles: Profiles[IO] }
-  trait RanksEnv { def ranks: Ranks[IO] }
-  trait RndEnv { def rnd: Rnd[IO] }
+  type LadderEnv   = Has[Ladder[IO]]
+  type ProfilesEnv = Has[Profiles[IO]]
+  type RanksEnv    = Has[Ranks[IO]]
+  type RndEnv      = Has[Rnd[IO]]
 
 }
