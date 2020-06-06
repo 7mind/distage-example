3fda913d8e374098a9326a8a567ee397734d0932
Kai <450507+neko-kai@users.noreply.github.com>
2020-06-06 10:34:24 +0100

    Update to izumi 0.10.12 (#74)

--- src/test/scala/leaderboard/PostgresDockerModule.scala
+++ src/test/scala/leaderboard/PostgresDockerModule.scala
@@ -1,20 +1,22 @@
 package leaderboard
 
 import distage.ModuleDef
-import leaderboard.config.PostgresPortCfg
-import izumi.distage.docker.Docker
 import izumi.distage.docker.Docker.DockerPort
 import izumi.distage.docker.examples.PostgresDocker
-import izumi.distage.docker.modules.DockerContainerModule
+import izumi.distage.docker.modules.DockerSupportModule
+import leaderboard.config.PostgresPortCfg
 import zio.Task
 
 object PostgresDockerModule extends ModuleDef {
+  // add docker support dependencies
+  include(DockerSupportModule[Task])
+
   // launch postgres docker for tests
   make[PostgresDocker.Container]
     .fromResource(PostgresDocker.make[Task])
 
-  // spawned docker container port is dynamic
-  // to prevent conflicts. Make PostgresPortCfg
+  // spawned docker container port is randomized
+  // to prevent conflicts, so make PostgresPortCfg
   // point to the new port. This will also
   // cause the container to start before
   // integration check is performed
@@ -23,19 +25,4 @@ object PostgresDockerModule extends ModuleDef {
       val knownAddress = docker.availablePorts.availablePorts(DockerPort.TCP(5432)).head
       PostgresPortCfg(knownAddress.hostV4, knownAddress.port)
   }
-
-  // add docker dependencies and override default configuration
-  include(new DockerContainerModule[Task] overridenBy new ModuleDef {
-    make[Docker.ClientConfig].from {
-      Docker.ClientConfig(
-        readTimeoutMs    = 60000,
-        connectTimeoutMs = 500,
-        allowReuse       = true,
-        useRemote        = false,
-        useRegistry      = true,
-        remote           = None,
-        registry         = None,
-      )
-    }
-  })
 }
--- src/test/scala/leaderboard/WiringTest.scala
+++ src/test/scala/leaderboard/WiringTest.scala
@@ -5,7 +5,7 @@ import distage.{DIKey, Injector}
 import izumi.distage.config.AppConfigModule
 import izumi.distage.model.definition.Activation
 import izumi.distage.model.definition.StandardAxis.Repo
-import izumi.distage.model.plan.GCMode
+import izumi.distage.model.plan.Roots
 import izumi.distage.testkit.scalatest.DistageBIOSpecScalatest
 import izumi.logstage.api.logger.LogRouter
 import leaderboard.plugins.{LeaderboardPlugin, ZIOPlugin}
@@ -26,7 +26,7 @@ final class WiringTest extends DistageBIOSpecScalatest[IO] {
               new LogstageModule(LogRouter.nullRouter, setupStaticLogRouter = false),
               new AppConfigModule(ConfigFactory.empty),
             ).merge,
-            GCMode(DIKey.get[LeaderboardRole[zio.IO]]),
+            Roots(DIKey[LeaderboardRole[IO]]),
           )
           .assertImportsResolvedOrThrow()
       }
