99b8176d3f24da2c4ed4dbd1b60fac8fffe42e4f
Kai <kai@7mind.io>
2020-08-18 10:39:47 +0100

    Scene doc wording

--- src/main/scala/leaderboard/axis/Scene.scala
+++ src/main/scala/leaderboard/axis/Scene.scala
@@ -4,11 +4,12 @@ import izumi.distage.model.definition.Axis
 
 /**
   * Choice axis controlling whether third-party services that the application requires
-  * should be provided by `distage-framework-docker` when the application starts (`Scene.Managed`),
-  * or whether it should try to connect to these services normally, assuming they already exist in the environment (`Scene.Provided`)
+  * should be provided by `distage-framework-docker` or another orchestrator when the application starts (`Scene.Managed`),
+  * or whether it should try to connect to these services as if they already exist in the environment (`Scene.Provided`)
   *
   * The set of third-party services required by the application is called a `Scene`, etymology being that the running
-  * third-party services that the application depends on are like a set-up scene which an actor (the application) then enters.
+  * third-party services that the application depends on are like a scene that is prepared for for the actor (the application)
+  * to enter.
   */
 object Scene extends Axis {
   case object Managed extends AxisValueDef
